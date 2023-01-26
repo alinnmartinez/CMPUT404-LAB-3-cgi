9c3b3fba7b24ee2842f05103597b6c39c2c5ce0c
Alinn <alinn1@ualberta.ca>
2023-01-26 11:25:10 -0700

    lab 3 finished

--- login.py
+++ login.py
@@ -13,18 +13,31 @@ password = s.getfirst("password")
 
 form_ok = username == secret.username and password == secret.password
 
+If not username and not password:
+        print(login_page())
+elif username == secret.username and password == secret.password:
+    print(secret_page(username, password))
+else:
+    print(login_page())
+    print("username and password:", username + ', ' + password)
+
 cookie = SimpleCookie(os.environ["HTTP_COOKIE]")
 cookie_username = None
 cookie_password = None
 if cookie.get("username"):
     cookie_username = cookie.get("username").value
 if cookie.get("password"):
-    cookie_password = get
+    cookie_password = cookie.get("password".value)
+
+cookie_ok = cookie_username  == secret.username and cookie_password == secret.password
+
+if cookie_ok:
+    username = cookie_username
+    password = cookie_password
+
 print("Content-Type: text/html")
 if form_ok:
+    print(f"Set-Cookie: username={username}")
+    print(f"Set-Cookie: password={password}")
 
 print()
-
-If not username and not password:
-    print(login_page())
-else 
